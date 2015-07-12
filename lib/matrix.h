d6f2165e312c524f653f10d9fe6a3bfd1ba0f8c0
Joe Haddad <jsh77@zips.uakron.edu>
2015-07-12 01:17:24 -0400

    Reorganize header

--- lib/matrix.h
+++ lib/matrix.h
@@ -22,11 +22,11 @@ void matrix_set(Matrix *matrix, int value);
 int matrix_prod(Matrix *matrix);
 int matrix_sum(Matrix *matrix);
 double matrix_double_sum(Matrix *matrix);
-void matrix_double_set(Matrix *matrix, double value);
-Matrix *matrix_sub_indices(Matrix *matrix, int row_start, int row_end, int col_start, int col_end);
-Matrix *matrix_sub_lists(Matrix *matrix, List *rows, List *cols);
-Matrix *matrix_sub_list_index(Matrix *matrix, List *rows, int col_start, int col_end);
-Matrix *matrix_sub_index_list(Matrix *matrix, int row_start, int row_end, List *cols);
+void matrix_dobule_set(Matrix *matrix, int value);
+Matrix *matrix_sub_indices(Matrix *o_matrix, int row_start, int row_end, int col_start, int col_end);
+Matrix *matrix_sub_lists(Matrix *o_matrix, List *rows, List *cols);
+Matrix *matrix_sub_list_index(Matrix *o_matrix, List *rows, int col_start, int col_end);
+Matrix *matrix_sub_index_list(Matrix *o_matrix, int row_start, int row_end, List *cols);
 Matrix *matrix_sub_col(Matrix *matrix, int col);
 Matrix *matrix_sub_row(Matrix *matrix, int row);
 Matrix *matrix_sub_concat_rows(Matrix *matrix, Matrix *rows);
@@ -36,6 +36,11 @@ List *matrix_to_list(Matrix *matrix);
 List *matrix_double_to_list(Matrix *matrix);
 Matrix *matrix_from_array(int rows, int cols, int arr[]);
 Matrix *matrix_from_list(List *l);
+void matrix_delete(Matrix *matrix);
+void matrix_scrap(Matrix *matrix);
+void *matrix_element_n_dim(Matrix *m, Matrix *ind, Matrix *dims);
+void matrix_display(Matrix *matrix);
+void matrix_double_display(Matrix *matrix);
 Matrix *matrix_add(Matrix *a, Matrix *b);
 void matrix_add_in(Matrix *a, Matrix *b);
 Matrix *matrix_double_add(Matrix *a, Matrix *b);
@@ -46,12 +51,4 @@ Matrix *matrix_double_subtract(Matrix *m1, Matrix *m2);
 Matrix *matrix_lgamma(Matrix *m);
 Matrix *matrix_create_sz(Matrix *matrix);
 
-void matrix_delete(Matrix *matrix);
-void matrix_scrap(Matrix *matrix);
-
-void *matrix_element_n_dim(Matrix *m, Matrix *ind, Matrix *dims);
-
-// for testing ease
-void matrix_display();
-void matrix_double_display();
 #endif
