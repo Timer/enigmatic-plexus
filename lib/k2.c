db6c66dff8721588f3e135a8e118f31e1b34bae3
Joe Haddad <jsh77@zips.uakron.edu>
2015-07-15 19:02:33 -0400

    Use DBL min.

--- lib/k2.c
+++ lib/k2.c
@@ -104,11 +104,10 @@ Matrix *prob_node(CPD *cpd, Matrix *self_ev, Matrix *pev) {
 double log_prob_node(CPD *cpd, Matrix *self_ev, Matrix *pev) {
   double score = 0;
   Matrix *p = prob_node(cpd, self_ev, pev);
-  double log_dbl_min = -700;
   double **data = (double **) p->data;
   for (int i = 0; i < p->rows * p->cols; ++i, ++data) {
     double d = **data;
-    score += d <= 0 ? log_dbl_min : log(d);
+    score += d <= 0 ? DBL_MIN : log(d);
   }
   matrix_delete(p);
   return score;
