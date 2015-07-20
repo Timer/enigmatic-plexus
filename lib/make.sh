0bafa6a3ed5751f09fd5052aecad483a81c69079
Joe Haddad <jsh77@zips.uakron.edu>
2015-07-20 01:53:21 -0400

    Remove benchmark native from darwin systems

--- lib/make.sh
+++ lib/make.sh
@@ -13,6 +13,9 @@ fi
 echo "... using $CC."
 rm *.out
 $CC -std=gnu99 -fopenmp -lmpi -c $(find . -name \*.c) -lm
+if [ $OS == "Darwin" ]; then
+  rm benchmark-native.o
+fi
 echo "Building..."
 $CC -std=gnu99 -fopenmp -lmpi $(find . -name \*.o -not -name k2.o) -o test.out -lm
 $CC -std=gnu99 -fopenmp -lmpi $(find . -name \*.o -not -name test.o) -o k2.out -lm
