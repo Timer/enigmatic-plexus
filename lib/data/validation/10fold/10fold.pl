82f4e2230be80334eb7e2775cd30076a4cc2a5ed
Awesome McAwesome <aed27@zips.uakron.edu>
2015-10-13 16:14:31 -0500

    10 fold test

--- lib/data/validation/10fold/10fold.pl
+++ lib/data/validation/10fold/10fold.pl
@@ -13,12 +13,14 @@ for (my $i= 0; $i < 10; ++$i) {
 
     my $trainfile = "train.csv";
     my $testfile = "test.csv";
-    my $pbsfile = "data.pbs";
+    my $testpbsfile = "test.pbs";
+    my $trainpbsfile = "train.pbs";
 
     open my $in, '<', $infile or die "Cannot open $infile: $!";
     open my $train, '>', "$existingdir/$trainfile" or die "Cannot open $trainfile: $!";
     open my $test, '>', "$existingdir/$testfile" or die "Cannot open $testfile: $!";
-    open my $pbs, '>', "$existingdir/$pbsfile" or die "Cannot open $pbsfile: $!";
+    open my $testpbs, '>', "$existingdir/$testpbsfile" or die "Cannot open $testpbsfile: $!";
+    open my $trainpbs, '>', "$existingdir/$trainpbsfile" or die "Cannot open $trainpbsfile: $!";
     
 
     while( my $line = <$in>)  {
@@ -29,15 +31,23 @@ for (my $i= 0; $i < 10; ++$i) {
 	}
     }
 
-    print $pbs "#!/bin/bash\n";
-    print $pbs "#PBS -l nodes=52:ppn=32\n";
-    print $pbs "#PBS -l walltime=00:30:00\n";
-    print $pbs "cd \$PBS_O_WORKDIR\n\n";
+    print $testpbs "#!/bin/bash\n";
+    print $testpbs "#PBS -l nodes=52:ppn=32\n";
+    print $testpbs "#PBS -l walltime=00:30:00\n";
+    print $testpbs "cd \$PBS_O_WORKDIR\n\n";
 
-    print $pbs "time aprun -n 52 -d 32 ./k2.out -t 64 -d data.csv -p 32 -m 5\n";
+    print $testpbs "time aprun -n 52 -d 32 ./k2.out -t 64 -d test.csv -p 32 -m 5 -o test_out.csv\n";
+
+    print $trainpbs "#!/bin/bash\n";
+    print $trainpbs "#PBS -l nodes=52:ppn=32\n";
+    print $trainpbs "#PBS -l walltime=00:30:00\n";
+    print $trainpbs "cd \$PBS_O_WORKDIR\n\n";
+
+    print $trainpbs "time aprun -n 52 -d 32 ./k2.out -t 64 -d train.csv -p 32 -m 5 -o train_out.csv\n";
     
     close $in;
     close $train;
     close $test;
-    close $pbs;
+    close $testpbs;
+    close $trainpbs;
 }
