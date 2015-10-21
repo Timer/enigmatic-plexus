cb9769c8d024cc2e4e96cb44a8669b4562f8b291
Awesome McAwesome <aed27@zips.uakron.edu>
2015-10-21 07:59:53 -0500

    added next 3 samples

---
+++ lib/data/samples/819200/10fold.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+my $infile = 'olddata.csv';
+my $outfile = 'data.csv';
+
+open my $in, '<', $infile or die "Cannot open $infile: $!";
+open my $out, '>', $outfile or die "Cannot open $outfile: $!";
+
+while( my $line = <$in>)  {
+    print $out $line;
+    print $out $line;
+    print $out $line;
+    print $out $line;
+    print $out $line;
+    print $out $line;
+    print $out $line;
+    print $out $line;
+}
+
+close $in;
+
